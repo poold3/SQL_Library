1f12e49ed639bbacc70a012942adc970a1f61555
Dallin Pool <dallinpool@gmail.com>
2022-06-30 12:46:05 -0600

    Updated notes

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "SQLLibrary.h"
+
+using namespace std;
+
+int main () {
+    SQL_Query("CREATE TABLE table.txt (id INT, name TEXT)");
+
+
+
+
+    return 0;
+}
