c23f44fa2cf22f122aeac1c9ba9cf4376470cfce
Dallin Pool <dallinpool@gmail.com>
2022-06-30 19:55:15 -0600

    Operation identification

---
+++ Makefile
@@ -0,0 +1,5 @@
+compile:
+	g++ -Wall -Werror -std=c++2a -g *.cpp
+
+run:
+	./a.out
--- SQLLibrary.h
+++ SQLLibrary.h
@@ -1,9 +1,44 @@
 #ifndef SQLLIBRARY_H
 #define SQLLIBRARY_H
 
+#include<string>
+#include<iostream>
+
+using namespace std;
+
+void Throw_Error(string message) {
+    throw std::invalid_argument(message);
+}
+
+string DecimalToHex(string decimalNumber) {
+
+}
+
+string HexToDecimal(string hexNumber) {
+    
+}
+
 void SQL_Query(string query) {
+    //Determine what operation is being performed
+    if (query.substr(0, 13) == "CREATE TABLE ") {
 
+    }
+    else if (query.substr(0, 7) == "SELECT ") {
 
+    }
+    else if (query.substr(0, 12) == "INSERT INTO ") {
+
+    }
+    else if (query.substr(0, 7) == "UPDATE ") {
+
+    }
+    else if (query.substr(0, 12) == "DELETE FROM ") {
+
+    }
+    else {
+        Throw_Error("Invalid SQL Operation!");
+    }
+    return;
 }
 
 
